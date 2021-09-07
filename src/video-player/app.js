2ce4766e03987b3f25471fc8b3fc184b73e22be1
Graham Small <graham.small@ert.com>
2021-09-07 09:09:39 +0100

    Add video player

---
+++ src/video-player/app.js
@@ -0,0 +1,58 @@
+/* Select elements */
+const videoPlayer = document.querySelector('.player');
+const video = videoPlayer.querySelector('.viewer');
+const progress = videoPlayer.querySelector('.progress');
+const progressBar = videoPlayer.querySelector('.progress__filled');
+const togglePlayBtn = videoPlayer.querySelector('.toggle');
+const skipBtns = videoPlayer.querySelectorAll('[data-skip]');
+const ranges = videoPlayer.querySelectorAll('.player__slider');
+
+/* app logic */
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+    return;
+  }
+  video.pause();
+}
+
+function updateButton() {
+  togglePlayBtn.textContent = video.paused ? '❚ ❚' : '►';
+}
+
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleSlider() {
+  video[this.name] = this.value;
+}
+
+function handleProgress() {
+  const progress = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${progress}%`
+}
+
+function updateProgress(e) {
+  const progressTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = progressTime;
+}
+
+
+/* event listeners */
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
+togglePlayBtn.addEventListener('click', togglePlay);
+
+skipBtns.forEach( btn => btn.addEventListener('click', skip));
+
+ranges.forEach( slider => slider.addEventListener('change', handleSlider ))
+
+progress.addEventListener('click', updateProgress);
+let mousedown = false;
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mousedown', () => mouseup = false);
+progress.addEventListener('mousemove', (e) => mousedown && updateProgress(e));
